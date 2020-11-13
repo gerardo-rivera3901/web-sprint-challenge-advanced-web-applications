6bcbbfe733495da80db882d50b62c04e2396088e
Gerardo Rivera <gerardo.rivera3901@gmail.com>
2020-11-12 23:08:52 -0800

    Completed MVP

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
-import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithauth";
 
 const initialColor = {
   color: "",
   code: { hex: "" }
 };
 
-const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+const ColorList = ({ colors }) => {
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -18,13 +17,15 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+    axiosWithAuth().put(`/colors/${colorToEdit.id}`, colorToEdit)
+      .then(() => window.location.reload())
+      .catch(err => console.log(err))
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+    axiosWithAuth().delete(`/colors/${color.id}`)
+      .then(() => window.location.reload())
+      .catch(err => console.log(err))
   };
 
   return (
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios'
+import { useHistory } from "react-router-dom";
+
+const initialState = {
+  username: '',
+  password: ''
+}
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+  const [loginData, setLoginData] = useState(initialState)
+  const { push } = useHistory()
+
+  const onChange = (event) => {
+    setLoginData({
+      ...loginData,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    axios.post('http://localhost:5000/api/login', loginData)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload)
+        push('/protected')
+      })
+  }
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div>
+      <form onSubmit={onSubmit}>
+        <h1>Welcome to the Bubble App!</h1>
+        <input 
+          type='text'
+          name='username'
+          placeholder='Enter Your Username'
+          value={loginData.username}
+          onChange={onChange}
+        />
+        <input 
+          type='password'
+          name='password'
+          placeholder='Enter Your Password'
+          value={loginData.password}
+          onChange={onChange}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
   );
 };
 
